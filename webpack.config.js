ad40533926b17aecd8e0719e9056a3d75d80e0a0
Alexey Konkin <79003704+AAKonkin@users.noreply.github.com>
2024-11-21 16:15:20 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
@@ -8,22 +7,5 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
-  },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//         template: path.resolve(__dirname, 'index.html'),
-//     })
-//   ],
-//   module: {
-//     rules: [{
-//         test: /\.m?js$/,
-//         exclude: /{node_modules|bower_components}/,
-//         use: {
-//             loaded: 'babel-loader',
-//             options: {
-//                 presets: ['@babel/preset-env']
-//             }
-//         }
-//     }]
-//   }
+  }
 };
